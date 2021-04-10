fcae2ca490f6b94675d7f5b1365ceeca6b392659
Qwerrty574 <ilyinsky.igor2010@yandex.ru>
2021-04-10 23:20:58 +0300

    upd task3 part1 50%

--- task3/struct_tmp.py
+++ task3/struct_tmp.py
@@ -11,7 +11,7 @@ class Struct:
 
     def fill(self, value):
         for i in range(1, self.s_size + 1):
-            self.struct['{}{}'.format(self.name, i)] = value[i-1]
+            self.struct['{}{}'.format(self.name, i)] = (value[i-1] if (isinstance(value[i-1], dict) or isinstance(value[i-1], list)) else str(value[i-1]))
 
     def build(self):
         return self.struct
