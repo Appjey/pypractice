0f22b2a1c1b17d75e8feeccdcb8e48c843c8b90b
Qwerrty574 <ilyinsky.igor2010@yandex.ru>
2021-04-12 02:12:51 +0300

    split task3

--- task3/struct_tmp.py
+++ task3/struct_tmp.py
@@ -9,9 +9,22 @@ class Struct:
         keys = ['{}{}'.format(self.name, i) for i in range(1, self.s_size + 1)]
         self.struct = dict.fromkeys(keys)
 
+    @staticmethod
+    def retype(value):
+        if isinstance(value, dict) or isinstance(value, list):
+            return value
+        elif isinstance(value, int):
+            return int(value)
+        elif isinstance(value, float):
+            return float(value)
+        elif isinstance(value, bytes):
+            return value.decode("utf-8")
+        else:
+            return str(value)
+
     def fill(self, value):
         for i in range(1, self.s_size + 1):
-            self.struct['{}{}'.format(self.name, i)] = (value[i-1] if (isinstance(value[i-1], dict) or isinstance(value[i-1], list)) else str(value[i-1]))
+            self.struct['{}{}'.format(self.name, i)] = self.retype(value[i - 1])
 
     def build(self):
         return self.struct
